317bad77e7a09cf98735121f1984ffeec569dd8a
juanfcarrillo <juanfi444@outlook.com>
2023-10-18 09:27:29 -0500

    feat(main): Add sumstars to main

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import lastUpdated from "./last-updated/lastUpdated.js"
 import listMoreStars from "./more-than-five/listMoreStarts.js"
+import sumAllStars from "./sum-starts/sumAllStars.js"
 
 export default async function main(getRepos) {
     const repos = await getRepos()
@@ -8,7 +9,9 @@ export default async function main(getRepos) {
 
     const lastUpdatedRepos = lastUpdated(repos)
 
-    // console.log(moreThanFive)
+    const sumStars = sumAllStars(repos)
+
     console.log(lastUpdatedRepos)
     console.log(moreThanFive)
+    console.log(sumStars)
 }
