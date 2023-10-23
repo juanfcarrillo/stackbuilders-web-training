8f0c9ea54ec67ad506802e0ffb28bebb68b58ddb
juanfcarrillo <juanfi444@outlook.com>
2023-10-23 12:39:03 -0500

    feat(main): added module 4 exercises

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import lastUpdated from "./last-updated/lastUpdated.js"
+import listAlphabetically from "./list-alphabetically/listAlphabetically.js"
 import { GhRepo } from "./models/GhRepo.js"
 import listMoreStars from "./more-than-five/listMoreStarts.js"
 import sumAllStars from "./sum-starts/sumAllStars.js"
+import topMoreThanFive from "./top-more-than-five/topMoreThanFive.js"
 
 export default async function main(getRepos: () => Promise<GhRepo[]>) {
     const repos = await getRepos()
@@ -12,7 +14,13 @@ export default async function main(getRepos: () => Promise<GhRepo[]>) {
 
     const sumStars = sumAllStars(repos)
 
+    const topFive = topMoreThanFive(repos)
+
+    const filterAlphaH = listAlphabetically(repos)
+
     console.log(lastUpdatedRepos)
     console.log(moreThanFive)
     console.log(sumStars)
+    console.log(topFive)
+    console.log(filterAlphaH)
 }
